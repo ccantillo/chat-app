6e00d51bed1058ac7d246e6934cfbdae07cc6031
carlos.cantillo@soluntech.com <carlos.cantillo@soluntech.com>
2022-11-26 21:15:11 -0500

    changed cors

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename)
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://vue-node-chat-app.netlify.app/',
+    origin: 'https://vue-node-chat-app.netlify.app',
   },
 })
 
